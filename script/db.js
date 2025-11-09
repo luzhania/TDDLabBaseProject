90567157c377942bdb67ee695c799b7892c342a4
Luzhania Cespedes Arancibia <luzhaniacesar@gmail.com>
2025-11-09 19:26:54 -0400

    fix: trying to connect to neon db

--- script/db.js
+++ script/db.js
@@ -1,9 +1,17 @@
-// db.js
-import pkg from 'pg';
+import pkg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const { Pool } = pkg;
+
+console.log("🔍 DATABASE_URL:", process.env.DATABASE_URL); // <-- debug temporal
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // obligatorio en Neon
+  },
 });
 
 export default pool;
